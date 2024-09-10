5afd7d06a46f2a194a285e37fb0b4d09335611a4
Zandy26 <blackninja01198@gmail.com>
2024-09-10 20:44:28 +0800

    method modulus

--- Calculator.java
+++ Calculator.java
@@ -16,7 +16,7 @@ public class Calculator {
     }	
 
 
-    // Method for Division
+    	// Method for Division
     public int divide(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Cannot divide by zero!");
@@ -24,10 +24,21 @@ public class Calculator {
         return a / b;
     }
 
+	   // Method for Modulus
+    public int modulus(int a, int b) {
+        return a % b;
+    }
+	
 
-
-
-
+	  // Main method to test the calculator
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Subtraction: " + calc.subtract(10, 5));
+        System.out.println("Multiplication: " + calc.multiply(10, 5));
+        System.out.println("Division: " + calc.divide(10, 5));
+        System.out.println("Modulus: " + calc.modulus(10, 5));
+    }
 
 
 
